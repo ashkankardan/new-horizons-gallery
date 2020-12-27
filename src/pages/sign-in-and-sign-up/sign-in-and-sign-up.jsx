1086d7d8667108552742cc56e4879f5a4ec96160
Ashkan Kardan <ashkankardan14@gmail.com>
2020-12-27 05:39:14 +0300

    signin and collection page added

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/Sign-In'
+
+import './sign-in-and-sign-up.scss';
+
+const SignInAndSignUp = () => (
+
+  <div className="sign-in-and-sign-up">
+  <SignIn />
+
+
+  </div>
+
+
+)
+
+export default SignInAndSignUp;
