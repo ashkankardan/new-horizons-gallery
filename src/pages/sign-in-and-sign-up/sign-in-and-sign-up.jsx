375e877bd62278a3c3338711320f6b01dfb923ff
Ashkan Kardan <ashkankardan14@gmail.com>
2020-12-28 10:58:14 +0300

    signup with email and password

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/Sign-In'
+import SignUp from '../../components/sign-up/Sign-Up'
 
 import './sign-in-and-sign-up.scss';
 
@@ -8,6 +9,7 @@ const SignInAndSignUp = () => (
 
   <div className="sign-in-and-sign-up">
   <SignIn />
+  <SignUp />
 
 
   </div>
